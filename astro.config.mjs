a259651bd2f13f48178390e95a92186470c22b5a
dacrab <vaggeliskavouras6@gmail.com>
2025-12-29 19:34:57 +0200

    fix: sort imports in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
